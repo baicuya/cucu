04aff4f306452d4187824854776b9cd40492de9c
baicuya <81811911+baicuya@users.noreply.github.com>
2022-03-18 15:18:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
-print('hello everyone')
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+def func():
+    print('hello everyone')
+    chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+    return st.line_chart(chart_data)
+    
+if __name__== '__main__':
+    func()
